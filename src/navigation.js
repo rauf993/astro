d37bf04f14e3a7392317416527dbddd889b4c5f1
rauf993 <rauf.hamden5056@alumnos.udg.mx>
2024-05-22 11:38:54 -0600

    hola

--- src/navigation.js
+++ src/navigation.js
@@ -12,61 +12,30 @@ export const headerData = {
       text: 'informacion',
       links: [
         {
-          text: 'Features (Anchor Link)',
+          text: 'Soccer Stars',
           href: getPermalink('/#features'),
         },
         {
-          text: 'Services',
-          href: getPermalink('/services'),
+          text: 'Historia',
+          href: getPermalink('/#features2 '),
         },
         {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
+          text: 'Avances Tecnologicos ',
+          href: getPermalink('/#Avances Tecnologicos'),
         },
         {
-          text: 'About us',
-          href: getPermalink('/about'),
+          text: 'Videojuegos',
+          href: getPermalink('/#videogames'),
         },
+       
         {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
+          text: 'Mejor jugador de futbol',
+          href: getPermalink('/#cr7'),
         },
       ],
     },
    
-    {
-      text: 'Video Games',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-startedwebsite-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
+   
     
   ],
 
@@ -130,8 +99,5 @@ export const footerData = {
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  `,
+
 };
